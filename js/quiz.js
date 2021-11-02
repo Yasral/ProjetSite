fd7629d07e63801c16a339caab8f43e0de0fdd16
Seynabou12 <dioneseynabou0@gmail.com>
2021-11-02 11:06:27 +0100

    utilisation des fonctions

--- js/quiz.js
+++ js/quiz.js
@@ -1,253 +1,87 @@
 window.onload = function () {
-    var temperature = ["temperature", "Question1", "Quelle est votre temperature?????"];
-    var fievre = ["fievre", "Question2", "Avez-vous de la fievre?????"];
-    var mt = ["maux de tete", "Question3", "Avez-vous mal à la tete?????"];
-    var dr = ["difficultés respiratoires", "Question4", "Avez-vous es difficultés respiratoires?????"];
-    var mg = ["mal de gorge", "Question5", "Avez-vous des mals de gorge??????"];
-    var diar = ["diarrhée", "Question6", "Avez-vous de la diarrhée??????"];
     var quest = 1;
     var num = document.getElementById("num");
-
+    var tab =
+        [
+            ["temperature", "Question1", "Quelle est votre temperature?????", [
+                '<input type="radio" name="r" value="Temperature > 36°C"/> <label>Temperature > 36°C</label> <input type="radio" name="r" value="Temperature < 36°C"/> <label>Temperature < 36°C</label> <input type="radio" name="r" value="36.7°C<Temperature< 37°C"/> <label>36.7°C<Temperature< 37°C</label>'
+            ]
+            ],
+            ["fievre", "Question2", "Avez-vous de la fievre?????", [
+                '<input type="radio" name="r" value="1"/> <label>Oui</label> <input type="radio" name="r" value="0"/> <label>Non</label>'
+            ]
+            ],
+            ["maux de tete", "Question3", "Avez-vous mal à la tete?????", [
+                '<input type="radio" name="r" value="1"/> <label>Oui</label> <input type="radio" name="r" value="0"/> <label>Non</label>'
+            ]
+            ],
+            ["difficultés respiratoires", "Question4", "Avez-vous es difficultés respiratoires?????", [
+                '<input type="radio" name="r" value="1"/> <label>Oui</label> <input type="radio" name="r" value="0"/> <label>Non</label>'
+            ]
+            ],
+            ["mal de gorge", "Question5", "Avez-vous des mals de gorge??????", [
+                '<input type="radio" name="r" value="1"/> <label>Oui</label> <input type="radio" name="r" value="0"/> <label>Non</label>'
+            ]
+            ],
+            ["diarrhée", "Question6", "Avez-vous de la diarrhée??????", [
+                '<input type="radio" name="r" value="1"/> <label>Oui</label> <input type="radio" name="r" value="0"/> <label>Non</label>'
+            ]
+            ]
+        ];
+    changeT();
     var temp = document.querySelector("#temperature");
     temp.onclick = function () {
         changeBC();
-        question = document.getElementById("question");
-
-        quest = 1; 
-        num.textContent = (quest+"/6");
-        //vider le contenu
-        question.textContent = " ";
-        var texte = document.createTextNode(temperature[1]);
-        question.appendChild(texte);
- 
-        para = document.getElementById("para");
-        para.textContent = " ";
-        var paragraphe = document.createTextNode(temperature[2]);
-        para.appendChild(paragraphe);
-
-        var choix1 = document.createElement("input");
-        choix1.setAttribute('type', 'checkbox');
-        choix1.setAttribute('name', 'r');
-        choix1.setAttribute('value', 'Temperature > 36°C');
-        var label1 = document.createElement("label");
-        label1.textContent = "Temperature > 36°C";
-
-        var choix2 = document.createElement("input");
-        choix2.setAttribute('type', 'checkbox');
-        choix2.setAttribute('name', 'r');
-        choix2.setAttribute('value', 'Temperature < 36°C');
-        var label2 = document.createElement("label");
-        label2.textContent = "Temperature < 36°C";
-
-        var choix3 = document.createElement("input");
-        choix3.setAttribute('type', 'checkbox');
-        choix3.setAttribute('name', 'r');
-        choix3.setAttribute('value', '36.7°C<Temperature< 37°C');
-        var label3 = document.createElement("label");
-        label3.textContent = "36.7°C<Temperature< 37°C";
-
-        form = document.getElementById("form");
-        form.textContent = " ";
-        form.appendChild(choix1);
-        form.appendChild(label1);
-        form.appendChild(choix2);
-        form.appendChild(label2);
-        form.appendChild(choix3);
-        form.appendChild(label3);
+        quest = 1;
+        changeT();
     };
     var fiev = document.querySelector("#fievre");
     fiev.onclick = function () {
         changeBC();
-        quest = 2; 
-        num.textContent = (quest+"/6");
-        //recuperer l'element 
-        question = document.getElementById("question");
-        //vider le contenu 
-        question.textContent = " ";
-        //créer un nouveau contenu
-        var texte = document.createTextNode(fievre[1]);
-        question.appendChild(texte);
-
-        para = document.getElementById("para");
-        para.textContent = " ";
-        var paragraphe = document.createTextNode(fievre[2]);
-        para.appendChild(paragraphe);
-
-        var opt1 = document.createElement("input");
-        opt1.setAttribute('type', 'checkbox');
-        opt1.setAttribute('name', 'r');
-        opt1.setAttribute('value', 'OUI');
-        var label1 = document.createElement("label");
-        label1.textContent = "OUI";
-
-        var opt2 = document.createElement("input");
-        opt2.setAttribute('type', 'checkbox');
-        opt2.setAttribute('name', 'r');
-        opt2.setAttribute('value', 'NON');
-        var label2 = document.createElement("label");
-        label2.textContent = "NON";
-
-        form = document.getElementById("form");
-        form.textContent = " ";
-        form.appendChild(opt1);
-        form.appendChild(label1);
-        form.appendChild(opt2);
-        form.appendChild(label2); 
+        quest = 2;
+        changeT();
     };
 
     var mauTt = document.querySelector("#mauTete");
     mauTt.onclick = function () {
         changeBC();
-        quest = 3; 
-        num.textContent = (quest+"/6");
-        question = document.getElementById("question");
-        question.textContent = " ";
-        var texte = document.createTextNode(mt[1]);
-        question.appendChild(texte);
-
-        para = document.getElementById("para");
-        para.textContent = " ";
-        var paragraphe = document.createTextNode(mt[2]);
-        para.appendChild(paragraphe);
-
-        var opt = document.createElement("input");
-        opt.setAttribute('type', 'checkbox');
-        opt.setAttribute('name', 'r');
-        opt.setAttribute('value', 'OUI');
-        var label1 = document.createElement("label");
-        label1.textContent = "OUI";
-
-
-        var opt0 = document.createElement("input");
-        opt0.setAttribute('type', 'checkbox');
-        opt0.setAttribute('name', 'r');
-        opt0.setAttribute('value', 'NON');
-        var label2 = document.createElement("label");
-        label2.textContent = "NON";
-
-        form = document.getElementById("form");
-        form.textContent = " ";
-        form.appendChild(opt);
-        form.appendChild(label1);
-        form.appendChild(opt0);
-        form.appendChild(label2);
+        quest = 3;
+        changeT();
     };
     var difr = document.querySelector("#difResp");
     difr.onclick = function () {
         changeBC();
-        quest = 4; 
-        num.textContent = (quest+"/6");
-        question = document.getElementById("question");
-        question.textContent = " ";
-        var texte = document.createTextNode(dr[1]);
-        question.appendChild(texte);
-
-        para = document.getElementById("para");
-        para.textContent = " ";
-        var paragraphe = document.createTextNode(dr[2]);
-        para.appendChild(paragraphe);
-
-        var opt = document.createElement("input");
-        opt.setAttribute('type', 'checkbox');
-        opt.setAttribute('name', 'r');
-        opt.setAttribute('value', 'OUI');
-        var label1 = document.createElement("label");
-        label1.textContent = "OUI";
-
-        var opt0 = document.createElement("input");
-        opt0.setAttribute('type', 'checkbox');
-        opt0.setAttribute('name', 'r');
-        opt0.setAttribute('value', 'NON');
-        var label2 = document.createElement("label");
-        label2.textContent = "NON";
-
-        form = document.getElementById("form");
-        form.textContent = " ";
-        form.appendChild(opt);
-        form.appendChild(label1);
-        form.appendChild(opt0);
-        form.appendChild(label2); 
+        quest = 4;
+        changeT();
     };
     var malgorge = document.querySelector("#malg");
     malgorge.onclick = function () {
         changeBC();
-        quest = 5; 
-        num.textContent = (quest+"/6");
-        question = document.getElementById("question");
-        question.textContent = " ";
-        var texte = document.createTextNode(mg[1]);
-        question.appendChild(texte);
-
-        para = document.getElementById("para");
-        para.textContent = " ";
-        var paragraphe = document.createTextNode(mg[2]);
-        para.appendChild(paragraphe);
-
-        var opt = document.createElement("input");
-        opt.setAttribute('type', 'checkbox');
-        opt.setAttribute('name', 'r');
-        opt.setAttribute('value', 'OUI');
-        var label1 = document.createElement("label");
-        label1.textContent = "OUI";
-
-        var opt0 = document.createElement("input");
-        opt0.setAttribute('type', 'checkbox');
-        opt0.setAttribute('name', 'r');
-        opt0.setAttribute('value', 'NON');
-        var label2 = document.createElement("label");
-        label2.textContent = "NON";
-
-        form = document.getElementById("form");
-        form.textContent = " ";
-        form.appendChild(opt);
-        form.appendChild(label1);
-        form.appendChild(opt0);
-        form.appendChild(label2); 
+        quest = 5;
+        changeT();
     };
 
     var diarrhe = document.querySelector("#diarr");
     diarrhe.onclick = function () {
         changeBC();
-        quest = 6; 
-        num.textContent = (quest+"/6");
-        question = document.getElementById("question");
-        question.textContent = " ";
-        var texte = document.createTextNode(diar[1]);
-        question.appendChild(texte);
-
-        para = document.getElementById("para");
-        para.textContent = " ";
-        var paragraphe = document.createTextNode(diar[2]);
-        para.appendChild(paragraphe);
-
-        var opt = document.createElement("input");
-        opt.setAttribute('type', 'checkbox');
-        opt.setAttribute('name', 'r');
-        opt.setAttribute('value', 'OUI');
-        var label1 = document.createElement("label");
-        label1.textContent = "OUI";
-
-        var opt0 = document.createElement("input");
-        opt0.setAttribute('type', 'checkbox');
-        opt0.setAttribute('name', 'r');
-        opt0.setAttribute('value', 'NON');
-        var label2 = document.createElement("label");
-        label2.textContent = "NON";
-
-        form = document.getElementById("form");
-        form.textContent = " ";
-        form.appendChild(opt);
-        form.appendChild(label1);
-        form.appendChild(opt0);
-        form.appendChild(label2); 
+        quest = 6;
+        changeT();
     };
     const suivant = document.getElementById("suiv");
-    suivant.addEventListener('click',() => {
-        changeBC();
+    suivant.addEventListener('click', () => {
+        // let recap = document.getElementsByTagName('r');
+        // recap 
+        changeBC(); 
         quest = quest >= 6 ? 1 : quest + 1; //equation ternaire
-        num.textContent = (quest+"/6");
-      
+        changeT();
+
     });
+    function changeT() {
+        document.getElementById("question").textContent = tab[quest - 1][1];
+        document.getElementById("para").textContent = tab[quest - 1][2];
+        document.getElementById("form").innerHTML = tab[quest - 1][3]
+        num.textContent = (quest + "/6");
+    }
 
     function changeBC() {
         document.getElementById('temperature').style.color = quest == 1 ? "blue" : ""
